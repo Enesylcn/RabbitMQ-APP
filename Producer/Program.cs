9375d490900d5ff38e6ca15691405ef10c027ebe
Enesylcn <enesylcn57@outlook.com>
2024-01-18 11:37:48 +0300

    first commit

---
+++ Producer/Program.cs
@@ -0,0 +1,23 @@
+﻿using System.Text;
+using RabbitMQ.Client;
+
+var factory = new ConnectionFactory { HostName = "localhost" };
+
+using var connection = factory.CreateConnection();
+
+using var channel = connection.CreateModel();
+
+channel.QueueDeclare(
+    queue: "letterbox",
+    durable: false,
+    exclusive: false,
+    autoDelete: false,
+    arguments: null);
+
+var message = "This is my first Message";
+
+var encodedMessage = Encoding.UTF8.GetBytes(message);
+
+channel.BasicPublish("", "letterbox", null, encodedMessage);
+
+Console.WriteLine($"Published message: {message}");
---
+++ consumer.js
@@ -0,0 +1,25 @@
+const amqp = require("amqplib");
+
+const queueName = process.argv[2] || "jobsQueue";
+
+connect_rabbitmq();
+
+async function connect_rabbitmq() {
+    try {
+        const connection = await amqp.connect("amqp://localhost:5672");
+        const channel = await connection.createChannel();
+        const assertion = channel.assertQueue(queueName);
+
+        // Mesajın Alınması...
+        console.log("Mesaj bekleniyor..");
+        channel.consume(queueName, (message) => {
+            console.log("Message", message.content.toString());
+            channel.ack(message)
+        });
+
+    } catch (error) {
+        console.log("Error", error);
+    }
+
+
+}
